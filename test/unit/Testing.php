792869c7e8c1726cdb48e881e2ce8c9235eff015
Andreas Panjaitan <andreaspanjaitan89@gmail.com>
2023-02-19 10:59:21 +0700

    Add Testing Schenario

--- test/unit/Testing.php
+++ test/unit/Testing.php
@@ -18,7 +18,7 @@ class Testing extends PHPUnit_Framework_TestCase {
         $TekenAjaApi = new \TekenAja\Api($this->apiKeyTest);        
         $selfieImage = dirname(__DIR__, 1)."/images_test/ghozali-jual-foto-selfie-di-nft-dapat-rp12-6-miliar-ternyata-begini-cara-kerjanya-USz4uJ39yh.jpeg";
         $ktpImage = dirname(__DIR__, 1)."/images_test/l-img20210420015823jpg20210420005933.jpeg";        
-        $tekenAja = $TekenAjaApi->register('andreas@singa.id','Andreas',1,'1989-05-03','jakarta','3175050305890008','085156229923','jl. trikora raya no. 14','13760',$ktpImage, $selfieImage);
+        $tekenAja = $TekenAjaApi->register('email@domain.com','nama',1,'1989-01-01','jakarta','1234567890000000','081234567890','Your address','12345',$ktpImage, $selfieImage);
         $responseData = json_decode($tekenAja->raw_body, true);        
         if(isset($responseData['status']) && isset($responseData['ref_id']) && isset($responseData['code']) &&  isset($responseData['timestamp']) && isset($responseData['message'])){
             return true;
@@ -29,7 +29,7 @@ class Testing extends PHPUnit_Framework_TestCase {
 
     public function testRegisterCheck() {        
         $TekenAjaApi = new \TekenAja\Api($this->apiKeyTest);        
-        $tekenAja = $TekenAjaApi->registerCheck('3175050305890008','check_nik','andreas@singa.id');
+        $tekenAja = $TekenAjaApi->registerCheck('1234567890000000','check_nik','email@domain.com');
         $responseData = json_decode($tekenAja->raw_body, true);        
         if(isset($responseData['status']) && isset($responseData['ref_id']) && isset($responseData['code']) &&  isset($responseData['timestamp']) && isset($responseData['message'])){
             return true;
